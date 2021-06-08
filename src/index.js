427bd495dfdc3446944ac9e728a558b08e66ac94
Tommy Palmer <hi@tommyp.org>
2021-06-08 07:42:22 +0100

    add webpack watch

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './style.css';
 function component() {
   const element = document.createElement('div');
 
+  console.log('hello')
+
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,15 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output"
+      title: "Development"
     })
   ],
   module: {
