bf4b12c8ea0d8f34b6a4d62492bec98001631dd3
Tommy Palmer <hi@tommyp.org>
2021-06-13 12:41:52 +0100

    set query param for location

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import objectToQuery from './utils/objectToQuery';
 const form = document.querySelector('form');
 const input = document.querySelector('input#search');
 
+const updateQueryParams = (searchParams) => {
+  window.history.pushState(null, null, `${window.location.pathname}?${searchParams}`);
+};
+
 const handleSubmit = async (e) => {
   e.preventDefault();
   const queryParams = objectToQuery({
     location: input.value,
   });
+
+  updateQueryParams(queryParams);
 
   const resp = await fetch(`/.netlify/functions/search?${queryParams}`, {
     // method: 'POST',
