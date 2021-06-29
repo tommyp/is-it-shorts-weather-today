99f13e3bd5d5f1ea899e0fe07320d858cf159494
Tommy Palmer <hi@tommyp.org>
2021-06-29 22:30:57 +0100

    add canvas and move forecast to a different js file

---
+++ src/forecast.js
@@ -0,0 +1,113 @@
+import objectToQuery from './utils/objectToQuery';
+
+const form = document.querySelector('form');
+const input = document.querySelector('#search input');
+const geoButton = document.querySelector('#geo_search');
+const decisionEl = document.querySelector('#decision');
+let data;
+let decision = 'no';
+let currentTemp;
+let condition;
+const trigger = 18;
+
+const updateQueryParams = (searchParams) => {
+  window.history.pushState(null, null, `${window.location.pathname}?${searchParams}`);
+};
+
+const warmTemp = (data) => {
+  const { temp_max: tempMax, temp } = data.main;
+  currentTemp = temp;
+
+  if (tempMax >= trigger) {
+    currentTemp = tempMax;
+    return true;
+  }
+  if (temp >= trigger) {
+    currentTemp = temp;
+    return true;
+  }
+};
+
+const renderDecision = () => {
+  decisionEl.innerHTML = `
+   <h2>${decision}</h2>
+   <p>${condition} / ${currentTemp.toPrecision(2)}</p>
+  `;
+};
+
+const setDecision = () => {
+  const code = data.weather[0].id;
+
+  const warm = warmTemp(data);
+
+  condition = data.weather[0].description;
+
+  if (code >= 800 && code < 804 && warm) {
+    decision = 'yes';
+  } else {
+    decision = 'no';
+  }
+
+  renderDecision();
+};
+
+const makeRequest = async (params) => {
+  const queryParams = objectToQuery(params);
+  updateQueryParams(queryParams);
+  const resp = await fetch(`/.netlify/functions/search?${queryParams}`, {
+    // method: 'POST',
+  });
+  data = await resp.json();
+  setDecision(data);
+};
+
+const geoQuery = async (lat, lon) => {
+  await makeRequest({ lat, lon });
+  input.value = data.name;
+};
+
+const locationQuery = (location) => {
+  makeRequest({
+    location,
+  });
+};
+
+const setPosition = async (position) => {
+  const { latitude, longitude } = position.coords;
+  await geoQuery(latitude, longitude);
+};
+
+const handleGeo = (e) => {
+  e.preventDefault();
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(setPosition);
+  } else {
+    console.log('no geo');
+  }
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  locationQuery(input.value);
+};
+
+geoButton.addEventListener('click', handleGeo);
+form.addEventListener('submit', handleSubmit);
+
+const hydrateFromParams = () => {
+  const url = new URL(window.location.href);
+  const params = new URLSearchParams(url.search);
+  console.log('hydration');
+
+  const location = params.get('location');
+  const lat = params.get('lat');
+  const lon = params.get('lon');
+  if (location) {
+    locationQuery(location);
+    input.value = location;
+  } else if (lat && lon) {
+    geoQuery(lat, lon);
+  }
+};
+
+hydrateFromParams();
