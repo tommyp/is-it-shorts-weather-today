089b0f5186e4ff21b957773c4d5c0a9bb6ce9c56
Tommy Palmer <hi@tommyp.org>
2021-06-30 07:13:37 +0100

    correctly size canvas

--- src/background.js
+++ src/background.js
@@ -5,4 +5,21 @@ const canvas = document.querySelector('canvas#background');
 
 const sandbox = new GlslCanvas(canvas);
 
+const calcSize = () => {
+  const ww = window.innerWidth;
+  const wh = window.innerHeight;
+  const dpi = window.devicePixelRatio;
+
+  const s = Math.max(ww + 400, wh);
+
+  canvas.width = s * dpi;
+  canvas.height = s * dpi;
+  canvas.style.width = `${s}px`;
+  canvas.style.height = `${s}px`;
+};
+
+calcSize();
+
+window.addEventListener('resize', calcSize);
+
 sandbox.load(shader);
