8a86d59248ee23966e0f4ba2390f0f5462c2f3d9
Tommy Palmer <hi@tommyp.org>
2021-07-21 09:46:11 +0100

    set preview card hardcoded path

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,14 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
       },
     ],
   },
