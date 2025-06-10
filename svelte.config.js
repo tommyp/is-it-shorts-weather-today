74fb096c430acd78b9e8ce61816dac64ecd7e388
Tommy Palmer <hi@tommyp.org>
2025-06-10 20:13:49 +0100

    add wrangler config

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,22 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			// See below for an explanation of these options
+			config: undefined,
+			platformProxy: {
+				configPath: undefined,
+				environment: undefined,
+				persist: undefined
+			},
+			fallback: 'plaintext',
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
+	}
 };
 
 export default config;
